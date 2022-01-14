f9a51ea2e46e5ad0e280f236628a47aaa4f25742
jaisonluizaliev <jaison.alieve@gmail.com>
2022-01-14 20:07:54 -0300

    Create Cart Screen

---
+++ pages/cart.js
@@ -0,0 +1,121 @@
+import React, { useContext } from 'react';
+import Layout from '../components/Layout';
+import { Store } from '../utils/Store';
+import NextLink from 'next/link';
+import Image from 'next/image';
+import {
+  Button,
+  Card,
+  Grid,
+  Link,
+  List,
+  ListItem,
+  MenuItem,
+  Select,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@material-ui/core';
+
+export default function CartScreen() {
+  const { state } = useContext(Store);
+  const {
+    cart: { cartItems },
+  } = state;
+
+  return (
+    <Layout title="Carrinho de Compras">
+      <Typography component="h1" variant="h1">
+        Carrinho de Compras
+      </Typography>
+      {cartItems.length === 0 ? (
+        <div>
+          Carrinho Vazio <NextLink href="/">Retornar as compras</NextLink>
+        </div>
+      ) : (
+        <Grid container spacing={1}>
+          <Grid item md={9} xs={12}>
+            <TableContainer>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Imagem</TableCell>
+                    <TableCell>Nome</TableCell>
+                    <TableCell align="right">Quantidade</TableCell>
+                    <TableCell align="right">Preço</TableCell>
+                    <TableCell align="right">Tirar Do Carrinho</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {cartItems.map((item) => (
+                    <TableRow key={item._id}>
+                      <TableCell>
+                        <NextLink href={`/product/${item.slug}`} passHref>
+                          <Link>
+                            <Image
+                              src={item.image}
+                              alt={item.name}
+                              width={50}
+                              height={50}
+                            />
+                          </Link>
+                        </NextLink>
+                      </TableCell>
+                      <TableCell>
+                        <NextLink href={`/product/${item.slug}`} passHref>
+                          <Link>
+                            <Typography>{item.name}</Typography>
+                          </Link>
+                        </NextLink>
+                      </TableCell>
+                      <TableCell align="right">
+                        <Select value={item.quantity}>
+                          {[...Array(item.countInStock).keys()].map((x) => (
+                            <MenuItem key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </MenuItem>
+                          ))}
+                        </Select>
+                      </TableCell>
+                      <TableCell align="right">{item.price}</TableCell>
+                      <TableCell align="right">
+                        <Button variant="contained" color="secondary">
+                          x
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Grid>
+          <Grid md={3} xs={12}>
+            <Card>
+              <List>
+                <ListItem>
+                  <Typography variant="h2">
+                    SubTotal ({cartItems.reduce((a, c) => a + c.quantity, 0)}
+                    {'  '}itens){' '}
+                    {cartItems
+                      .reduce((a, c) => a + c.quantity * c.price, 0)
+                      .toLocaleString('pt-br', {
+                        style: 'currency',
+                        currency: 'BRL',
+                      })}
+                  </Typography>
+                </ListItem>
+                <ListItem><Button variant="contained" fullWidth color="primary">
+                      Finalizar Compra
+                  </Button></ListItem>
+              </List>
+            </Card>
+          </Grid>
+        </Grid>
+      )}
+    </Layout>
+  );
+}
